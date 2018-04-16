5deda4af561ead480e632b2afe6d4af851b614f7
yankj12 <yankj12@163.com>
2018-04-17 01:41:22 +0800

    公共类中，处理boolean属性的get方法时没有处理方法名为is开头的情况
    fix #7

--- src/main/java/com/yan/common/util/SchameCopyUtil.java
+++ src/main/java/com/yan/common/util/SchameCopyUtil.java
@@ -20,8 +20,15 @@ public class SchameCopyUtil {
 					for(Method sourceMethod : sourceMethods){
 						String methodName = sourceMethod.getName();
 						//遍历get方法
-						if(methodName.startsWith("get")){
-							String fieldName = methodName.substring(3, 4).toLowerCase() + methodName.substring(4);
+						if(methodName.startsWith("get") || methodName.startsWith("is")){
+							String fieldName = null;
+							
+							if(methodName.startsWith("get")) {
+								fieldName = methodName.substring(3, 4).toLowerCase() + methodName.substring(4);
+							}else if(methodName.startsWith("is")) {
+								fieldName = methodName.substring(2, 3).toLowerCase() + methodName.substring(3);
+							}
+							
 							Class returnType = sourceMethod.getReturnType();
 							//这个是用来处理带泛型的的类型
 							Type type = sourceMethod.getGenericReturnType();
@@ -33,7 +40,13 @@ public class SchameCopyUtil {
 								//避免两个类中字段不一致的时候，因为找不到对应的set方法，报空指针
 								
 								//向target类中set值
-								String setterMethodName = "set" + methodName.substring(3);
+								String setterMethodName = null;
+								if(methodName.startsWith("get")) {
+									setterMethodName = "set" + methodName.substring(3);
+								}else if(methodName.startsWith("is")) {
+									setterMethodName = "set" + methodName.substring(2);
+								}
+								
 								try {
 									Method targetSetterMethod = targetClazz.getMethod(setterMethodName, returnType);
 									targetSetterMethod.invoke(target, value);
